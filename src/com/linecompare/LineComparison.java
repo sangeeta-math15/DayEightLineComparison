1bed9a1b0cb9991e283054d2c906b2bd99ea8803
sngtmth <sngtmth@gmail.com>
2021-10-03 13:27:21 +0530

    Added: Welcome Message to Line Comparison Program

---
+++ src/com/linecompare/LineComparison.java
@@ -0,0 +1,16 @@
+package com.linecompare;
+
+/**
+ * Line Comparison Computation Program
+ * @author CHANDRU
+ *
+ */
+public class LineComparison {
+
+	public static void main(String[] args) {
+
+		System.out.print("Welcome to Line Comparison Computation Program on Master Branch");
+
+	}
+
+}
